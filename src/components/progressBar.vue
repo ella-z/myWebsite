2522714fe7f9908ac980a1dea4e7d2b2f75a9c88
ella-z <hgdsvafj@126.com>
2020-03-03 22:54:35 +0800

    up

--- src/components/progressBar.vue
+++ src/components/progressBar.vue
@@ -1,15 +1,101 @@
 <template>
-    
+  <div id="progressBar">
+    <ul>
+      <li v-for="(item,index) in skillBar" :key="index" :class="item.title">
+        {{item.title}}
+        <span>{{item.count}}%</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        
+  props: ["skillBar"],
+  data() {
+    return {
+      count:0
+    };
+  },
+  methods: {
+    addKeyframe() {
+      //给进度条添加动画样式
+      let style = document.createElement("style"); // 创建<style></style>标签
+      this.skillBar.forEach(element => {
+        const keyframe = `@keyframes ${element.title}{
+                    from{
+                        width:0%;
+                    }
+                    to{
+                        width:${element.count}%;
+                    }
+                }`;
+        let text = document.createTextNode(`
+                .${element.title}::after{ animation-name:${element.title};} 
+                ${keyframe}`); // 添加伪类样式
+        style.appendChild(text); // 把伪类样式添加样式表
+      });
+      document.body.appendChild(style);
+    },  
+    addCount(){
+      for(let i=0;i<2;i++){
+        setInterval(()=>{
+          //console.log(1);
+        },1000)
+      }
     }
-}
+  },
+  mounted() { 
+    this.addCount();
+    this.addKeyframe();
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+#progressBar {
+  li {
+    list-style: none;
+    position: relative;
+    z-index: 1;
+    font-size: 1.3vw;
+    margin-bottom: 11%;
+    span{
+        font-size: 1.3vw; 
+        font-weight: 700;
+        background: linear-gradient(135deg, #454e93, #ff7b4d);
+        background-clip: text;
+        color: transparent;
+    }
+  }
+  li::before,
+  li::after {
+    content: "";
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 1.5vw;
+    border-radius: 10px;
+    background: linear-gradient(90deg, #454e93, #ff7b4d);
+    opacity: 0.5;
+    bottom: -1.9vw;
+  }
+  /* li::before{
+    height: 1.7vw;
+    bottom:-2vw;
+    left: -0.15vw;
+    filter: blur(0.2vw);                                   //way1：使用高斯模糊做阴影
+    background: linear-gradient(90deg,#454E93,#FF7B4D);
+    opacity: 0.8;
 
+    box-shadow: 0 0 1vw #454E93;  //way2：直接使用box-shadow
+}*/
+  li::after {
+    opacity: 1;
+    background-image: linear-gradient(90deg, #454e93, #ff7b4d);
+    animation-duration: 1.2s;
+    animation-fill-mode: forwards;
+    animation-timing-function: ease-out;
+  }
+}
 </style>
--- src/views/aboutMe/aboutMe.vue
+++ src/views/aboutMe/aboutMe.vue
@@ -1,63 +1,63 @@
 <template>
-    <div class="aboutMe">
-        <areaHeader :headerTitle='headerTitle' :headerLogo='headerLogo'></areaHeader>
-        <div class="me-content">
-           <div class="self-intro">
-                <ul>
-                    <li v-for="(item,index) in selfIntroData" :key="index"> 
-                        <span :class="{'span-show':item.isShow}" @click="selfIntroClick(item,index)">{{item.title}}</span>
-                        <p :class="{'p-show':item.isShow}">{{item.article}}</p>
-                    </li>
-                </ul>
-                <div class="self-icon">
-                    <div class="self-icon-outer" v-for='(item,index) in selfIconData' :key="index">
-                        <i class="iconfont self-icon-img"  v-html="item.icon" @mouseenter="mouseEnter(item)" @mouseleave="mouseLeave(item)">{{item.icon}}</i>
-                        <span class="self-icon-content"   :class="{'show':item.isShow}">
-                            {{item.content}}
-                        </span>
-                    </div>
-                </div>
-
-            </div>
-            <div class="self-photo">
-                <div class="self-photo-outer"></div>
-                <div class="self-photo-inner">
-                    <img src="../../assets/m.jpg" alt="">
-                </div>
-            </div>
-            <div class="skill-data">
-                <span>我的技能</span>
-                <div class="skill-data-bar">
-                    <ul>
-                        <li class="js">JS</li>
-                        <li 
-                        v-for="(item,index) in skillBar" 
-                        :key='index' 
-                        
-                        >{{item.title}}</li>
-                    </ul>
-                </div>
-            </div>
+  <div class="aboutMe">
+    <areaHeader :headerTitle="headerTitle" :headerLogo="headerLogo"></areaHeader>
+    <div class="me-content">
+      <div class="self-intro">
+        <ul>
+          <li v-for="(item,index) in selfIntroData" :key="index">
+            <span
+              :class="{'span-show':item.isShow}"
+              @click="selfIntroClick(item,index)"
+            >{{item.title}}</span>
+            <p :class="{'p-show':item.isShow}">{{item.article}}</p>
+          </li>
+        </ul>
+        <div class="self-icon">
+          <div class="self-icon-outer" v-for="(item,index) in selfIconData" :key="index">
+            <i
+              class="iconfont self-icon-img"
+              v-html="item.icon"
+              @mouseenter="mouseEnter(item)"
+              @mouseleave="mouseLeave(item)"
+            >{{item.icon}}</i>
+            <span class="self-icon-content" :class="{'show':item.isShow}">{{item.content}}</span>
+          </div>
         </div>
+      </div>
+      <div class="self-photo">
+        <div class="self-photo-outer"></div>
+        <div class="self-photo-inner">
+          <img src="../../assets/m.jpg" alt />
+        </div>
+      </div>
+      <div class="skill-data">
+        <span>我的技能</span>
+        <div class="skill-data-bar">
+          <progressBar :skillBar='skillBar'></progressBar>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import areaHeader from '../../components/areaHeader'
+import areaHeader from "../../components/areaHeader";
+import progressBar from "../../components/progressBar";
 
 export default {
-    components:{
-        areaHeader
-    },
-    data(){
-        return{
-            headerTitle:'About Me',
-            headerLogo:'&#xe68b;',
-            enterIndex:0,
-            selfIntroData:[
-                {
-                    title:'学生',
-                    article:`我是一名计算机专业的学生我是一名计算机专业的学生
+  components: {
+    areaHeader,
+    progressBar
+  },
+  data() {
+    return {
+      headerTitle: "About Me",
+      headerLogo: "&#xe68b;",
+      enterIndex: 0,
+      selfIntroData: [
+        {
+          title: "学生",
+          article: `我是一名计算机专业的学生我是一名计算机专业的学生
                              我是一名计算机专业的学生我是一名计算机专业的学生
                              我是一名计算机专业的学生我是一名计算机专业的学生
                              我是一名计算机专业的学生我是一名计算机专业的学生
@@ -66,99 +66,92 @@ export default {
                              我是一名计算机专业的学生我是一名计算机专业的学生
                              我是一名计算机专业的学生我是一名计算机专业的学生
                              我是一名计算机专业的学生我是一名计算机专业的学生`,
-                    isShow:true
-                },
-                {
-                    title:'前端',
-                    article:'前端',
-                    isShow:false
-                },
-                {
-                    title:'后台',
-                    article:'后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台',
-                    isShow:false
-                },
-                {
-                    title:'剪辑',
-                    article:'剪辑jjjjjjjjjjjjjjjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj',
-                    isShow:false 
-                },
-                {
-                    title:'游戏',
-                    article:'游戏lllllllllllllllllllllllllllllllll',
-                    isShow:false
-                }
-            ],
-            selfIconData:[
-                {
-                    icon:`&#xe695;`,
-                    content:'WeChat:enhei_0',
-                    isShow:false
-                },
-                {
-                    icon:`&#xe627;`,
-                    content:'QQ:2151633944',
-                    isShow:false
-                },
-                {
-                    icon:`&#xe600;`,
-                    content:'GitHub:ella-z',
-                    isShow:false
-                },
-                {
-                    icon:`&#xeac3;`,
-                    content:'Steam:柠檬奶盖',
-                    isShow:false
-                },
-            ],
-            skillBar:[
-                {
-                    title:'VUE'
-                },
-                {
-                    title:'HTML'
-                },
-                {
-                    title:'CSS'
-                }
-            ]
-        }
-    },
-    methods:{
-        mouseEnter(item){
-            item.isShow = true;
-        },                          //鼠标移入移出self-icon时，改变item的状态
-        mouseLeave(item){              
-            item.isShow = false;
+          isShow: true
         },
-        selfIntroClick(item,index){
-            this.selfIntroData[index].isShow = !this.selfIntroData[index].isShow;
-            this.selfIntroData.forEach(element => {
-                if(this.selfIntroData[index].title !== element.title){
-                    element.isShow = false;
-                }
-            });
+        {
+          title: "前端",
+          article: "前端",
+          isShow: false
         },
-        changeKeyframe(){
-            this.skillBar.forEach(element =>{
-                const keyframe = `@keyframes ${element.title}{
-                    from{
-                        width:0%;
-                    }
-                    to{
-                        width:50%
-                    }
-                }`;
-                console.log(keyframe);
-            });
+        {
+          title: "后台",
+          article:
+            "后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台后台",
+          isShow: false
+        },
+        {
+          title: "剪辑",
+          article:
+            "剪辑jjjjjjjjjjjjjjjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj剪辑jjjjjj",
+          isShow: false
+        },
+        {
+          title: "游戏",
+          article: "游戏lllllllllllllllllllllllllllllllll",
+          isShow: false
         }
+      ],
+      selfIconData: [
+        {
+          icon: `&#xe695;`,
+          content: "WeChat:enhei_0",
+          isShow: false
+        },
+        {
+          icon: `&#xe627;`,
+          content: "QQ:2151633944",
+          isShow: false
+        },
+        {
+          icon: `&#xe600;`,
+          content: "GitHub:ella-z",
+          isShow: false
+        },
+        {
+          icon: `&#xeac3;`,
+          content: "Steam:柠檬奶盖",
+          isShow: false
+        }
+      ],
+      skillBar: [
+        {
+          title: "VUE",
+          count: 30
+        },
+        {
+            title:"JS",
+            count:60
+        },
+        {
+          title: "HTML",
+          count: 70
+        },
+        {
+          title: "CSS",
+          count: 50
+        }
+      ]
+    };
+  },
+  methods: {
+    mouseEnter(item) {
+      item.isShow = true;
+    }, //鼠标移入移出self-icon时，改变item的状态
+    mouseLeave(item) {
+      item.isShow = false;
     },
-    mounted(){
-        this.changeKeyframe();
+    selfIntroClick(item, index) {
+      this.selfIntroData[index].isShow = !this.selfIntroData[index].isShow;
+      this.selfIntroData.forEach(element => {
+        if (this.selfIntroData[index].title !== element.title) {
+          element.isShow = false;
+        }
+      });
     }
-}
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import 'styles/aboutMe.scss';
+@import "styles/aboutMe.scss";
 </style>
